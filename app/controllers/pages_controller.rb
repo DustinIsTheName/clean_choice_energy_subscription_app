56d9273d16dff6303ee4fd6541bbbed7ff7cd31e
Dustin Milam <darklordsushi@yahoo.com>
2018-09-15 15:02:20 -0400

    installed devise gem and pages controller and views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+  def import
+  end
+
+  def subscription
+  end
+
+  def log
+  end
+
+  def users
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "pages#import"
+
+  controller :pages do
+    get 'import' => :import
+    get 'subscription' => :subscription
+    get 'log' => :log
+    get 'users' => :users
+  end
+
 end
