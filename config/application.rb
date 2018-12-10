cb89d8ddc822edb94fedabaa76ea1eb728e57cb4
Dustin Milam <darklordsushi@yahoo.com>
2018-12-10 15:20:15 -0500

    added config initialize_on_precompile

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module CleanChoiceEnergySubscriptionApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :delayed_job
+    config.assets.initialize_on_precompile = false
 
     ShopifyAPI::Base.site = "https://#{ENV["API_KEY"]}:#{ENV["PASSWORD"]}@#{ENV["SHOPIFY_URL"]}/admin"
   end
