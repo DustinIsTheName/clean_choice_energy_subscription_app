3c56b355ad51218117e2ca75bd0000d9209debac
Dustin Milam <darklordsushi@yahoo.com>
2018-12-11 10:39:27 -0500

    lowered zoom, increased pdf background size

--- app/mailers/gift_mailer.rb
+++ app/mailers/gift_mailer.rb
@@ -11,7 +11,7 @@ class GiftMailer < ApplicationMailer
     attachments['gift_certificate.pdf'] = WickedPdf.new.pdf_from_string(
       render_to_string('gifts/view_pdf.html.erb'),
       orientation: 'Landscape',
-      zoom: 1.6
+      zoom: 1.4
     )
 
     mail(to: @line_item["properties"].select{|p| p["name"] == "RecipientEmail"}.first["value"], from: 'cleanchoiceenergy@no-reply.com', subject: 'A gift for you')
