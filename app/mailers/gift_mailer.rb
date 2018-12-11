ef610b31a08f6804e46460d70ce52c2cbe9b4902
Dustin Milam <darklordsushi@yahoo.com>
2018-12-11 11:06:00 -0500

    edited font and zoom

--- app/mailers/gift_mailer.rb
+++ app/mailers/gift_mailer.rb
@@ -8,10 +8,16 @@ class GiftMailer < ApplicationMailer
 
     @line_item = line_item
 
+    if Rails.env.production?
+      zoom = 1.4
+    else
+      zoom = 9.0
+    end
+
     attachments['gift_certificate.pdf'] = WickedPdf.new.pdf_from_string(
       render_to_string('gifts/view_pdf.html.erb'),
       orientation: 'Landscape',
-      zoom: 1.4
+      zoom: zoom
     )
 
     mail(to: @line_item["properties"].select{|p| p["name"] == "RecipientEmail"}.first["value"], from: 'cleanchoiceenergy@no-reply.com', subject: 'A gift for you')
