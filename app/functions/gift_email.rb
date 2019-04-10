314a12e3824f8b8fd8a42fb39614f9a5c7e14952
Dustin Milam <darklordsushi@yahoo.com>
2019-04-10 17:56:53 -0400

    check for earth day email

--- app/functions/gift_email.rb
+++ app/functions/gift_email.rb
@@ -3,7 +3,7 @@ class GiftEmail
   def self.order_created(line_item, buyer_email, first_name, last_name)
     puts "order_created"
 
-    if line_item["properties"].select{|p| p["name"] == "RecipientEmail"}.size > 0
+    if line_item["properties"].select{|p| p["name"] == "RecipientEmail"}.size > 0 or line_item["title"].downcase.include? "earth day"
 
       if line_item["title"].downcase.include? "valentine"
         GiftMailer.valentine_gift_email(line_item).deliver
