b8c4469f0a3ea5ceb1ceb085bf67f76b2c74b026
Dustin Milam <darklordsushi@yahoo.com>
2019-04-15 15:54:23 -0400

    validate uniqueness for subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,8 @@ class Subscription < ApplicationRecord
   serialize :address, Hash
 
   validates :first_name, :last_name, :address, :cc_number, presence: true
+  validates_uniqueness_of :email, :allow_blank => true, :allow_nil => true
+  validates_uniqueness_of :cc_number, :scope => [:first_name, :last_name]
 
   self.per_page = 25
 end
